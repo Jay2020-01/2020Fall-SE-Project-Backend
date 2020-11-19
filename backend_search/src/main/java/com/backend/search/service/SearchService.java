831e72038d6e6c951b8859a39f386780fb4a28a7
Kazamori1 <2898904168@qq.com>
2020-11-19 13:54:17 +0800

    highlight

--- backend_search/src/main/java/com/backend/search/service/SearchService.java
+++ backend_search/src/main/java/com/backend/search/service/SearchService.java
@@ -11,13 +11,17 @@ import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.text.Text;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.elasticsearch.annotations.Highlight;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -49,6 +53,14 @@ public class SearchService {
 		else 
 			builder.query(QueryBuilders.termsQuery("title", keyword));
 		builder.timeout(new TimeValue(5, TimeUnit.SECONDS));
+
+		//高亮
+		HighlightBuilder highlightBuilder = new HighlightBuilder();
+		highlightBuilder.field("title");
+		highlightBuilder.requireFieldMatch(false);
+		highlightBuilder.preTags("<span style='color:red'>");
+		highlightBuilder.postTags("</span>");
+		builder.highlighter(highlightBuilder);
 		
 		searchRequest.source(builder);
         SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
@@ -56,7 +68,20 @@ public class SearchService {
         // 取出数据
         ArrayList<Map<String, Object>> list = new ArrayList<>();
         for (SearchHit documentFields : searchResponse.getHits().getHits()) {
-            list.add(documentFields.getSourceAsMap());
+
+        	Map<String, HighlightField> highlightFieldMap = documentFields.getHighlightFields();
+        	HighlightField title = highlightFieldMap.get("title");
+        	Map<String,Object> sourceAsMap = documentFields.getSourceAsMap();
+        	//解析高亮字段
+			if(title!=null){
+				Text[] fragments = title.fragments();
+				String new_title = "";
+				for(Text text : fragments){
+					new_title += text;
+				}
+				sourceAsMap.put("title",new_title);
+			}
+            list.add(sourceAsMap);
         }
         
         return list;
