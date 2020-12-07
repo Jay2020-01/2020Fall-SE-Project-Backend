57384441b41339171c106201f2958ebd65c30597
qxforever <qxforeverr@qq.com>
2020-12-07 21:35:26 +0800

    论文基础查询

---
+++ backend_server/src/main/java/com/backend/server/dao/PaperDao.java
@@ -0,0 +1,32 @@
+package com.backend.server.dao;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+import javax.annotation.Resource;
+
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Component;
+
+import com.backend.server.entity.Paper;
+
+@Component
+public class PaperDao {
+
+	@Resource
+	private MongoTemplate mongoTemplate;
+	
+	Paper findPaperById(Integer id) {
+		Query query = new Query(Criteria.where("id").is(id));
+		return mongoTemplate.findOne(query, Paper.class);
+	}
+	
+	List<Paper> Demo(String field, String context) {
+		Pattern pattern = Pattern.compile(".*?" + context + ".*"); 
+		Query query = new Query();
+		query.addCriteria(Criteria.where(field).regex(pattern));
+		return mongoTemplate.find(query, Paper.class);
+	}
+}
