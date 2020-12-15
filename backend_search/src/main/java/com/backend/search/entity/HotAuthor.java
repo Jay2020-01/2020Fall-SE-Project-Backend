ea9502d465844442a8d6340d1343acc2d237043d
Kazamori <54504504+Kazamori1@users.noreply.github.com>
2020-12-15 12:27:29 +0800

    Update HotAuthor.java

--- backend_search/src/main/java/com/backend/search/entity/HotAuthor.java
+++ backend_search/src/main/java/com/backend/search/entity/HotAuthor.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Data
 //@Document(collection = "c_h_author")
-public class HotAuthor implements Serializable {
+public class HotAuthor {
 
     @Id
     private String _id;
@@ -16,21 +16,8 @@ public class HotAuthor implements Serializable {
     private String aid;
     private String name;
     private Integer hIndex;
-    private List<tag> tags;
-    private List<pub> pubs;
     private Integer nPubs;
     private Integer nCitation;
-    private List<String> orgs;
-    private String position;
-    private List<String> pidDoc;
     private Integer userId;
-}
-class tag {
-    private Integer w;
-    private String t;
-}
-class pub {
-    private String i;
-    private Integer r;
 }
 
